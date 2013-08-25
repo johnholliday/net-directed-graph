0cde78151188fb203fafc45cc9617a4acbf1cb7e
Devendra Tewari <devendra.tewari@gmail.com>
2013-08-25 20:19:34 -0300

    Reorganize unit tests

--- DirectedGraphTest/DirectedGraphTest.cs
+++ DirectedGraphTest/DirectedGraphTest.cs
@@ -7,62 +7,87 @@ namespace DirectedGraphTest
     [TestClass]
     public class DirectedGraphTest
     {
-        /// <summary>
-        /// Test DirectedGraph. 
-        /// TODO: refactor this method into separate test methods for easier understanding and maintenance.
-        /// </summary>
-        [TestMethod]
-        public void TestDirectedGraph()
-        {
-            DirectedGraph<string, int> graph = new DirectedGraph<string, int>();
-            
-            // add several nodes
-            graph.AddNode("A", "B", 5);
-            graph.AddNode("B", "C", 4);
-            graph.AddNode("C", "D", 8);
-            graph.AddNode("D", "C", 8);
-            graph.AddNode("D", "E", 6);
-            graph.AddNode("A", "D", 5);
-            graph.AddNode("C", "E", 2);
-            graph.AddNode("E", "B", 3);
-            graph.AddNode("A", "E", 7);
+        DirectedGraph<string, int> graph;
 
+        [TestInitialize]
+        public void Initialize()
+        {
+            // add several nodes
+            graph = new DirectedGraph<string, int>();
+            graph.AddEdge("A", "B", 5);
+            graph.AddEdge("B", "C", 4);
+            graph.AddEdge("C", "D", 8);
+            graph.AddEdge("D", "C", 8);
+            graph.AddEdge("D", "E", 6);
+            graph.AddEdge("A", "D", 5);
+            graph.AddEdge("C", "E", 2);
+            graph.AddEdge("E", "B", 3);
+            graph.AddEdge("A", "E", 7);
+        }
+
+        [TestMethod]
+        public void TestAddEdge()
+        {            
             // test duplicate
             try
             {
-                graph.AddNode("A", "B", 5);
+                graph.AddEdge("A", "B", 5);
                 Assert.Fail("ArgumentException not thrown.");
             }
             catch(ArgumentException)
             {
             }
+        }
 
-            // test search
-
+        [TestMethod]
+        public void TestSearchAB()
+        {
             LinkedList<LinkedList<Node<string, int>>> results = graph.Search("A", "B", 1);
             printResults("A-B (Depth <= 1)", results);
             Assert.AreEqual(1, results.Length);
             Assert.AreEqual(2, results[0].Length);
             Assert.AreEqual("B", results[0][1].Value);
+        }
 
-            results = graph.Search("A", "C", 4);
+        [TestMethod]
+        public void TestSearchAC()
+        {
+            LinkedList<LinkedList<Node<string, int>>> results = graph.Search("A", "C", 4);
             printResults("A-C (Depth <= 4)", results);
+            Assert.AreEqual(6, results.Length);
+        }
 
-            results = graph.Search("A", "D", cycle:false);
+        [TestMethod]
+        public void TestSearchAD()
+        {
+            LinkedList<LinkedList<Node<string, int>>> results = graph.Search("A", "D", cycle: false);
             printResults("A-D", results);
+            Assert.AreEqual(4, results.Length);
+        }
 
-            results = graph.Search("B", "B");
+        [TestMethod]
+        public void TestSearchBB()
+        {
+            LinkedList<LinkedList<Node<string, int>>> results = graph.Search("B", "B");
             printResults("B-B", results);
+            Assert.AreEqual(2, results.Length);
+        }
 
-            results = graph.Search("C", "D", depth: 5);
+        [TestMethod]
+        public void TestSearchCD()
+        {
+            LinkedList<LinkedList<Node<string, int>>> results = graph.Search("C", "D", depth: 5);
             printResults("C-D (Depth <= 5)", results);
+            Assert.AreEqual(3, results.Length);
+        }
 
+        [TestMethod]
+        public void TestSearchCC()
+        {
             // test search with weight and cycle
-
-            results = graph.Search("C", "C", weight: 30, cycle: true);
+            LinkedList<LinkedList<Node<string, int>>> results = graph.Search("C", "C", weight: 30, cycle: true);
             Assert.AreEqual(7, results.Length);
             printResults("C-C (Weight < 30)", results);
-
         }
 
         /// <summary>
